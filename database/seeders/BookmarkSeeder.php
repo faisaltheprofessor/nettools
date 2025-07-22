bb916ff3e150e1f9a7229bb0e3a2a2d2711a8b38
Faisal Khan <faisalfazily@gmail.com>
2025-07-22 14:07:14 +0200

    remove seeder

--- database/seeders/BookmarkSeeder.php
+++ database/seeders/BookmarkSeeder.php
@@ -12,35 +12,5 @@ class BookmarkSeeder extends Seeder
      */
     public function run(): void
     {
-        $sg1 = Bookmark::create(['name' => 'SG1', 'type' => 'folder']);
-        $repo = Bookmark::create(['name' => 'Repositories', 'type' => 'folder', 'parent_id' => $sg1->id]);
-
-        Bookmark::create([
-            'name' => 'Checkmk',
-            'type' => 'link',
-            'url' => 'https://vs035.ba-pankow.verwalt-berlin.de/pankow/check_mk',
-            'parent_id' => $sg1->id,
-        ]);
-
-        Bookmark::create([
-            'name' => 'Repo1',
-            'type' => 'link',
-            'url' => 'http://repo01/',
-            'parent_id' => $repo->id,
-        ]);
-
-        Bookmark::create([
-            'name' => 'IP Datenbank - User',
-            'type' => 'link',
-            'url' => 'https://ip-admin.ba-pankow.verwalt-berlin.de/login?next=/',
-            'parent_id' => $sg1->id,
-        ]);
-
-        Bookmark::create([
-            'name' => 'Bitwarden',
-            'type' => 'link',
-            'url' => 'https://admin-vault.ba-pankow.verwalt-berlin.de',
-            'icon' => 'icons/bitwarden.png', // optional
-        ]);
     }
 }
