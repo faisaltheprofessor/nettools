a86a77b7d44dbd8df51fd828e1d65d8488535780
Faisal Khan <faisalfazily@gmail.com>
2025-10-08 15:11:25 +0200

    display a message in 403 page

--- resources/views/errors/partials/_403_content.blade.php
+++ resources/views/errors/partials/_403_content.blade.php
@@ -1,35 +1,55 @@
-<div class="relative flex items-center justify-center h-[70vh] overscroll-none">
-    <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-        <div class="flex flex-col items-center pt-8 sm:pt-0 text-center">
-            {{-- Code & Text (Laravel default style) --}}
-            <div class="flex items-center">
-                <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">403</div>
-                <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">Nicht Berechtigt</div>
+    @php
+        // SVG List
+        $images = [
+            'storage/undraw/1.svg',
+            'storage/undraw/2.svg',
+            'storage/undraw/3.svg',
+        ];
+        // Random SVG
+        $randomImage = asset($images[array_rand($images)]);
+    @endphp
+
+    <div class="relative flex items-center justify-center h-[80vh] overscroll-none">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 w-full">
+            {{-- Callout Hinweis  --}}
+            <div class="mb-8">
+                <flux:callout color="amber" icon="information-circle" class="flex flex-row text-left">
+                    Ich arbeite derzeit an den Berechtigungen. Bitte benutze vorübergehend die DHCP-Seite, um den DNS-Dienst neu zu starten. Diese Seite wird für Tests während der Entwicklung verwendet.
+                </flux:callout>
             </div>
 
-            {{-- SVG Illustration --}}
-            <div class="mt-8 w-64">
-                <img src="{{ asset('storage/undraw/unauthorized.svg') }}"
-                     alt="Unauthorized Illustration"
-                     class="w-full h-auto">
-            </div>
+            <div class="flex flex-col items-center pt-8 sm:pt-0 text-center">
 
-            {{-- Homepage button --}}
-            <div class="mt-8">
-                @if (Route::has('dashboard'))
-                    <flux:button href="{{ route('dashboard') }}" icon:trailing="home">
-                        Zur Startseite
-                    </flux:button>
-                @elseif (Route::has('home'))
-                    <flux:button href="{{ route('home') }}" icon:trailing="home">
-                        Zur Startseite
-                    </flux:button>
-                @else
-                    <flux:button href="/" icon:trailing="home">
-                        Zur Startseite
-                    </flux:button>
-                @endif
+                <div class="flex items-center">
+                    <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
+                        403
+                    </div>
+                    <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
+                        Nicht Berechtigt
+                    </div>
+                </div>
+
+                {{-- SVG --}}
+                <div class="mt-8 w-72">
+                    <img src="{{ $randomImage }}" alt="Unauthorized Illustration" class="w-full h-auto">
+                </div>
+
+                {{-- Homepage Button  --}}
+                <div class="mt-8">
+                    @if (Route::has('dashboard'))
+                        <flux:button href="{{ route('dashboard') }}" icon:trailing="home">
+                            Zur Startseite
+                        </flux:button>
+                    @elseif (Route::has('home'))
+                        <flux:button href="{{ route('home') }}" icon:trailing="home">
+                            Zur Startseite
+                        </flux:button>
+                    @else
+                        <flux:button href="/" icon:trailing="home">
+                            Zur Startseite
+                        </flux:button>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
-</div>
