3e67ce9f0b2b136cbdf1846addf516f78f73e988
Faisal Khan <faisalfazily@gmail.com>
2025-10-21 11:54:00 +0200

    favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nettools</title>
+    <title>Netzwerk Tools</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @vite("resources/css/app.css")
     @vite("resources/js/app.js")
     @fluxAppearance
